daec1ee57a70e4901f047de9d82e7229b7cea1f9
Daojim <jimmyddao@gmail.com>
2025-05-14 22:09:26 -0400

    Add dummy data

---
+++ src/data/todos.ts
@@ -0,0 +1,17 @@
+export const dummyData = [
+  {
+    id: 1,
+    title: "Watch YouTube tutorial",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Code along",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Finish project",
+    completed: false,
+  },
+];
