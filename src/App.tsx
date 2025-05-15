31c5d4d5b5da9c0d12572abb6189fdaccedb4cde
Daojim <jimmyddao@gmail.com>
2025-05-14 22:47:41 -0400

    Add styling to main and to-do list's background

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { dummyData } from "./data/todos";
 function App() {
   return (
     <>
-      <main className="py-10 bg-red-50 h-screen">
+      <main className="py-10 bg-red-50 h-screen space-y-5">
         <h1 className="font-bold text-3xl text-center">To-do List</h1>
-        <div className="max-w-lg mx-auto">
+        <div className="max-w-lg mx-auto bg-red-200 rounded-md p-5">
           <div className="space-y-2">
             {dummyData.map((todo) => (
               <TodoItem todo={todo} />
