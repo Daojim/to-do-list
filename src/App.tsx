366bf768e133729615ab8663d92ddacab8e0c306
Daojim <jimmyddao@gmail.com>
2025-05-14 22:30:48 -0400

    Add dummy data to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import "./App.css";
+import { dummyData } from "./data/todos";
 
 function App() {
   return (
     <>
       <main className="py-10 bg-red-50 h-screen">
         <h1 className="font-bold text-3xl text-center">To-do List</h1>
+        <div className="max-w-lg mx-auto">
+          <div className="space-y-2">
+            {dummyData.map((todo) => (
+              <p key={todo.id} className="text-lg">
+                {todo.title}
+              </p>
+            ))}
+          </div>
+        </div>
       </main>
     </>
   );
