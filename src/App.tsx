fa752bfc569b239432fb9d9d54b16b099ad93b4f
Daojim <jimmyddao@gmail.com>
2025-05-15 15:07:11 -0400

    Add toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(dummyData);
+
+  function handleToggle(id: number) {
+    console.log("Toggling:", id);
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  }
+
   return (
     <>
       <main className="py-10 bg-red-50 h-screen space-y-5">
         <h1 className="font-bold text-3xl text-center">To-do List</h1>
         <div className="max-w-lg mx-auto bg-red-200 rounded-md p-5">
           <div className="space-y-2">
-            {dummyData.map((todo) => (
-              <TodoItem todo={todo} />
+            {todos.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} onToggle={handleToggle} />
             ))}
           </div>
         </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,13 +2,19 @@ import type { Todo } from "../types/todo";
 
 interface TodoItemProps {
   todo: Todo;
+  onToggle: (id: number) => void;
 }
 
-function TodoItem({ todo }: TodoItemProps) {
+function TodoItem({ todo, onToggle }: TodoItemProps) {
   return (
     <div>
       <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-amber-300">
-        <input type="checkbox" className="scale-125" />
+        <input
+          type="checkbox"
+          className="scale-125"
+          checked={todo.completed}
+          onChange={() => onToggle(todo.id)}
+        />
         <span className={todo.completed ? "line-through text-gray-400" : ""}>
           {todo.title}
         </span>
