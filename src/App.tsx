9b8ea0f950d13a2a9fe8b4c96d0be143f4a242b3
Daojim <jimmyddao@gmail.com>
2025-05-14 22:43:46 -0400

    Add inputs and styling on inputs

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <div className="max-w-lg mx-auto">
           <div className="space-y-2">
             {dummyData.map((todo) => (
-              <TodoItem />
+              <TodoItem todo={todo} />
             ))}
           </div>
         </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,20 @@
-function TodoItem() {
-  return <div>TodoItem</div>;
+import type { Todo } from "../types/todo";
+
+interface TodoItemProps {
+  todo: Todo;
+}
+
+function TodoItem({ todo }: TodoItemProps) {
+  return (
+    <div>
+      <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-amber-300">
+        <input type="checkbox" className="scale-125" />
+        <span className={todo.completed ? "line-through text-gray-400" : ""}>
+          {todo.title}
+        </span>
+      </label>
+    </div>
+  );
 }
 
 export default TodoItem;
